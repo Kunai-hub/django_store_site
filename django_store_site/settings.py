9147c8e7c3297ddae7f1771655da6349a7a032db
kunai <neni23@mail.ru>
2024-08-15 16:53:42 +0300

    media url, media root added

--- django_store_site/settings.py
+++ django_store_site/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- products/models.py
+++ products/models.py
@@ -12,10 +12,10 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория {self.category.name}'
+        return f'Товар: {self.name} | Категория: {self.category.name}'
