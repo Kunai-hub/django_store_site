86361ced4f1e1f28a598471837f5a0a4483c2d5a
kunai <neni23@mail.ru>
2024-08-16 13:56:25 +0300

    users login, registration added w/o forms

--- django_store_site/settings.py
+++ django_store_site/settings.py
@@ -137,3 +137,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
--- django_store_site/urls.py
+++ django_store_site/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
