0ac13d3626cd0cded3c16b9e3704994c3347fecb
kunai <neni23@mail.ru>
2024-08-16 14:53:53 +0300

    users login added with form

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from users.views import login, registration
+from users.views import login_user, registration_user
 
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('registration/', registration, name='registration'),
+    path('login/', login_user, name='login'),
+    path('registration/', registration_user, name='registration'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+
+from users.forms import LoginForm
 
 
-def login(request):
+def login_user(request):
+
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('products:index'))
+    else:
+        form = LoginForm()
+
     context = {
         'title': 'Store - Авторизация',
+        'form': form,
     }
 
     return render(request, 'users/login.html', context=context)
 
 
-def registration(request):
+def registration_user(request):
     context = {
         'title': 'Store - Регистрация',
     }
