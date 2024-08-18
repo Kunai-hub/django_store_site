5a7fe251642059b73184815da61646272220ce87
kunai <neni23@mail.ru>
2024-08-18 17:26:43 +0300

    mixins for title, messages added

--- products/views.py
+++ products/views.py
@@ -4,21 +4,19 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
 from products.models import Product, ProductCategory, Basket
+from general.views import TitleMixin
 
 
-class IndexView(TemplateView):
+class IndexView(TitleMixin, TemplateView):
     template_name = 'products/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['title'] = 'Store'
-        return context
+    title = 'Store'
 
 
-class ProductListView(ListView):
+class ProductListView(TitleMixin, ListView):
     model = Product
     template_name = 'products/products.html'
     paginate_by = 3
+    title = 'Store - Каталог'
 
     def get_queryset(self, *args, **kwargs):
         queryset = super(ProductListView, self).get_queryset()
@@ -27,7 +25,6 @@ class ProductListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
-        context['title'] = 'Store - Каталог'
         context['categories'] = ProductCategory.objects.all()
         return context
 
--- users/views.py
+++ users/views.py
@@ -1,44 +1,39 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from users.forms import LoginForm, RegistrationForm, ProfileForm
 from products.models import Basket
 from users.models import User
+from general.views import TitleMixin
 
 
-class LoginUserView(LoginView):
+class LoginUserView(TitleMixin, LoginView):
     template_name = 'users/login.html'
     form_class = LoginForm
-
-    def get_context_data(self, **kwargs):
-        context = super(LoginUserView, self).get_context_data(**kwargs)
-        context['title'] = 'Store - Авторизация'
-        return context
+    title = 'Store - Авторизация'
 
 
-class RegistrationUserView(CreateView):
+class RegistrationUserView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     template_name = 'users/registration.html'
     form_class = RegistrationForm
     success_url = reverse_lazy('users:login')
-
-    def get_context_data(self, **kwargs):
-        context = super(RegistrationUserView, self).get_context_data(**kwargs)
-        context['title'] = 'Store - Регистрация'
-        return context
+    success_message = 'Регистрация прошла успешно!'
+    title = 'Store - Регистрация'
 
 
-class ProfileUserView(UpdateView):
+class ProfileUserView(TitleMixin, UpdateView):
     model = User
     template_name = 'users/profile.html'
     form_class = ProfileForm
+    title = 'Store - Профиль'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.request.user.id,))
 
     def get_context_data(self, **kwargs):
         context = super(ProfileUserView, self).get_context_data(**kwargs)
-        context['title'] = 'Store - Профиль'
         context['baskets'] = Basket.objects.filter(user=self.request.user)
         return context
