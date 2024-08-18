6a3bc1757d95c76453d330ce3dc73d39b3f3d945
kunai <neni23@mail.ru>
2024-08-18 14:42:13 +0300

    CBV for index added

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from products.views import index, products, basket_add, basket_remove
+from products.views import IndexView, products, basket_add, basket_remove
 
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('products/', products, name='products'),
     path('products/category/<int:category_id>/', products, name='products_category'),
     path('products/page/<int:page_number>/', products, name='products_page'),
