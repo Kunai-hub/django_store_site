e94603d051d245ef41ef91305d42a968f4a84197
kunai <neni23@mail.ru>
2024-08-20 17:23:27 +0300

    test for registration get added

--- users/tests.py
+++ users/tests.py
@@ -1,2 +1,17 @@
+from http import HTTPStatus as status
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RegistrationUserViewTest(TestCase):
+
+    def setUp(self):
+        self.path = reverse('users:registration')
+
+    def test_registration_user_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, status.OK)
+        self.assertTemplateUsed(response, 'users/registration.html')
+        self.assertEqual(response.context_data['title'], 'Store - Регистрация')
