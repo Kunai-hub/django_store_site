c07936eaf1b12246951b775494edb0a38882c1ca
kunai <neni23@mail.ru>
2024-08-20 16:47:00 +0300

    test for index added

--- products/tests.py
+++ products/tests.py
@@ -1,2 +1,15 @@
+from http import HTTPStatus as status
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTest(TestCase):
+
+    def test_index_view(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, status.OK)
+        self.assertTemplateUsed(response, 'products/index.html')
+        self.assertEqual(response.context_data['title'], 'Store')
