53cda4f9fbb9ad3b162efb97f507d6b1035eea5c
kunai <neni23@mail.ru>
2024-08-22 16:22:40 +0300

    orders create added with form

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+    fields = ('id', 'created_timestamp',
+              ('first_name', 'last_name'),
+              ('email', 'address'),
+              'basket_history', 'status', 'buyer'
+    )
+    readonly_fields = ('id', 'created_timestamp',)
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from orders.forms import OrderForm
 from general.views import TitleMixin
@@ -8,3 +9,8 @@ class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order_create.html'
     form_class = OrderForm
     title = 'Store - Оформление заказа'
+    success_url = reverse_lazy('orders:create')
+
+    def form_valid(self, form):
+        form.instance.buyer = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
