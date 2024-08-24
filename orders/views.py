d100249e902dda709bac08dd06234428056188bf
kunai <neni23@mail.ru>
2024-08-24 15:27:11 +0300

    stripe webhook added with static data

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,9 @@ from http import HTTPStatus as status
 
 import stripe
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
@@ -41,8 +42,39 @@ class OrderCreateView(TitleMixin, CreateView):
                     'quantity': 1,
                 },
             ],
+            metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:canceled')),
         )
         return HttpResponseRedirect(checkout_session.url, status=status.SEE_OTHER)
+
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_SECRET_WEBHOOK
+        )
+    except ValueError:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if (
+            event['type'] == 'checkout.session.completed'
+            or event['type'] == 'checkout.session.async_payment_succeeded'
+    ):
+        fulfill_checkout(event['data']['object'])
+
+    return HttpResponse(status=200)
+
+
+def fulfill_checkout(session):
+    print("Fulfilling Checkout Session", session)
